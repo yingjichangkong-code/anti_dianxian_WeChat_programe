835f5361b853ae2024c26ddcf94306154c79ba80
yingjichangkong-code <19836062191@163.com>
2025-06-04 11:09:47 +0800

    Add files via upload
    防癫痫

---
+++ miniprogram-7/utils/util.js
@@ -0,0 +1,14 @@
+const formatTime = date => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+
+  return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+};
+
+module.exports = {
+  formatTime: formatTime
+};
